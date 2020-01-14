f88f8f95e4bf95f51caa76df6f7aee4ca3b94c94
William Li <wul@microsoft.com>
2020-01-14 11:15:43 -0800

    Can deserialize both

--- src/dotnet/commands/dotnet-tool/search/NugetSearchSerializables.cs
+++ src/dotnet/commands/dotnet-tool/search/NugetSearchSerializables.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.DotNet.ToolPackage;
@@ -77,30 +79,24 @@ namespace Microsoft.DotNet.Tools.Tool.Search
         public int Downloads { get; }
     }
     
+    /// <summary>
+    /// Author field could be a string or a string array
+    /// </summary>
     internal class AuthorsConverter : JsonConverter<NugetSearchApiAuthorsSerializable>
     {
-        // public override NugetSearchApiAuthorsSerializable Read(
-        //     ref Utf8JsonReader reader,
-        //     Type typeToConvert,
-        //     JsonSerializerOptions options) =>
-        //     DateTimeOffset.ParseExact(reader.GetString(),
-        //         "MM/dd/yyyy", CultureInfo.InvariantCulture);
-        //
-        // public override void Write(
-        //     Utf8JsonWriter writer,
-        //     DateTimeOffset dateTimeValue,
-        //     JsonSerializerOptions options) =>
-        //     writer.WriteStringValue(dateTimeValue.ToString(
-        //         "MM/dd/yyyy", CultureInfo.InvariantCulture));
         public override NugetSearchApiAuthorsSerializable Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             if (reader.TokenType == JsonTokenType.StartArray)
             {
-                
+                var doc = JsonDocument.ParseValue(ref reader);
+                var resultAuthors = doc.RootElement.EnumerateArray().Select(author => author.GetString()).ToArray();
+                return new NugetSearchApiAuthorsSerializable() { Authors = resultAuthors };
             }
-
-            var s = reader.GetString();
-            return new NugetSearchApiAuthorsSerializable() { Authors = new string[] { s } };
+            else
+            {
+                var s = reader.GetString();
+                return new NugetSearchApiAuthorsSerializable() { Authors = new string[] { s } };
+            }
         }
 
         public override void Write(Utf8JsonWriter writer, NugetSearchApiAuthorsSerializable value, JsonSerializerOptions options)
